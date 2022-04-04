2436e62cbd3002eff013d45963a5c0f0bf380875
“RudiBob” <rudi.bob.harris@gmail.com>
2022-04-04 14:30:26 +1000

    added LatestWinsPreProcessor currently does nothing - add breakpoints and confirm flows

---
+++ src/LatestWins.Crawling/Processors/PreProcessing/LatestWinsPreProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CluedIn.Core;
+using CluedIn.Core.Data;
+using CluedIn.Core.Data.Parts;
+using CluedIn.Core.Processing;
+using CluedIn.Processing.Models.Utilities;
+
+namespace CluedIn.Crawling.LatestWins.Processors.PreProcessing
+{
+    public class LatestWinsPreProcessor : PropertyUtility<IEntityMetadataPart>, IMergePreProcessor
+    {
+        public bool Accepts(ExecutionContext context, IEnumerable<IEntityCode> codes)
+        {
+            return true;
+        }
+
+        public SaveResult Process(ExecutionContext context, IEntityMetadataPart metadataToMerge, IDataPart dataToMerge, Entity targetEntity)
+        {
+            if (dataToMerge == null)
+                return SaveResult.Ignored;
+
+            //metadataToMerge.Properties["mergeTimeStamp"] = DateTimeOffset.UtcNow.ToString();
+
+            return SaveResult.Success;
+        }
+    }
+}
