9a190a11b26f8b7eb695a5e8e8bfdbcfa5ffb6a1
“RudiBob” <rudi.bob.harris@gmail.com>
2022-04-05 08:55:05 +1000

    first cut deletes data parts - but only if we already had 2 parts attached

--- src/LatestWins.Crawling/Processors/PreProcessing/LatestWinsPreProcessor.cs
+++ src/LatestWins.Crawling/Processors/PreProcessing/LatestWinsPreProcessor.cs
@@ -7,6 +7,8 @@ using CluedIn.Core.Data;
 using CluedIn.Core.Data.Parts;
 using CluedIn.Core.Processing;
 using CluedIn.Processing.Models.Utilities;
+using CluedIn.Processing.Processors;
+using CluedIn.Processing.Processors.PreProcessing;
 
 namespace CluedIn.Crawling.LatestWins.Processors.PreProcessing
 {
@@ -23,6 +25,59 @@ namespace CluedIn.Crawling.LatestWins.Processors.PreProcessing
                 return SaveResult.Ignored;
 
             //metadataToMerge.Properties["mergeTimeStamp"] = DateTimeOffset.UtcNow.ToString();
+
+            if (targetEntity.EntityType != "/Person")
+                return SaveResult.Ignored;
+
+            // for this prototype - let's only operate on the "chris" named entity
+            if (targetEntity.Name == "chris")
+            {
+                // okay... we already have 2 data parts and we are thinking of adding a third...
+                // let's delete as many old data parts as possible
+
+                // TODO: we need to handle the case were we have only one existing data part... perhaps guard the second part as indicated?
+
+                //var parts = targetEntity.Details.Parts.OfType<DataPart>();
+                if (targetEntity.Details.DataEntries.Count == 2)
+                {
+                    var it = targetEntity.Details.DataEntries.GetEnumerator();
+                    it.MoveNext();
+                    var first = it.Current;
+                    it.MoveNext();
+                    var second = it.Current;
+
+                    if (first.ProcessedEntityData.ModifiedDate > second.ProcessedEntityData.ModifiedDate)
+                    {
+                        targetEntity.Details.DataEntries.Remove(second);
+                    }
+                    else
+                    {
+                        targetEntity.Details.DataEntries.Remove(first);
+                    }
+
+                    // TODO: put me in a Count == 1 clause?
+
+                    it = targetEntity.Details.DataEntries.GetEnumerator();
+                    it.MoveNext();
+                    first = it.Current;
+
+                    if (dataToMerge.ProcessedEntityData.ModifiedDate > first.ProcessedEntityData.ModifiedDate)
+                    {
+                        targetEntity.Details.DataEntries.Remove(first);
+                    }
+                    else
+                    {
+                        return SaveResult.Ignored;
+                    }
+
+                    {
+                        // this happens later in the common code
+                        //MappingProcessing.CreateMappings(targetEntity);
+                        //VersionHistoryProcessing.CreateChangeHistory(targetEntity);
+                        //VersionHistoryProcessing.CreateChangeHistoryEdges(targetEntity, context);
+                    }
+                }
+            }
 
             return SaveResult.Success;
         }
